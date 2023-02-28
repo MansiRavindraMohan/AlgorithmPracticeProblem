6855a4f91e8a851f860fd4af7e021d56df88508d
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-28 16:15:53 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmPracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data structure algorithm practice problem");
+        }
+    }
+}
